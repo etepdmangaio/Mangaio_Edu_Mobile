d86a8c84a319293f90b72bd1287caad256298d20
andreribeiro <prof.andrelbribeiro@gmail.com>
2025-04-22 14:12:43 -0300

    Segundo commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from sidebar_components.matematica import sidebar_matematica
 from sidebar_components.geografia import sidebar_geografia
 from sidebar_components.historia import sidebar_historia2
-from sidebar_components.Informacoes import sidebar_informacoes
+#from sidebar_components.Informacoes import sidebar_informacoes
 
 
 st.set_page_config(page_title="MangaioEdu", layout="wide")
@@ -16,4 +16,4 @@ sidebar_matematica()
 
 sidebar_geografia()
 
-sidebar_informacoes()
+#sidebar_informacoes()
